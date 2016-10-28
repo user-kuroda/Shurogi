06130eaac3b49a15fc02154563dd91bc9c99db70
Kuroda Moeko <kuroda-moeko@outlook.jp>
2016-10-28 10:24:19 +0900

    エラー表示とユーザー系諸々

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,16 @@
-class User < ActiveRecord::Base
+﻿class User < ActiveRecord::Base
  has_many :categories
  has_many :items
 
+ validates :userid,presence: true, format: { with: /\A[a-z0-9]+\z/i }
+ validates :name,presence: true
+ validates :pass,presence: true, format: { with: /\A[a-z0-9]+\z/i }
+ validates :mail,presence: true, format: { with: /\A[a-z0-9]+\z/i }
+ validates :call,presence: true
+ validates_uniqueness_of :userid, :message => "は既に使用されています"
+
  validates_confirmation_of :pass  
- attr_accessor             :pass_confirmation  
+ attr_accessor :pass_confirmation  
 
   def self.authenticate(userid, pass)
     usr = find_by(userid: userid)
